ef9ce1f92c342d8e9b050afa00629eb022f64ec2
Daniel P <danielpimen92@gmail.com>
2018-05-14 13:30:43 -0400

    updates

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -1,29 +1,36 @@
-import React from "react";
-import { Link, Route } from "react-router-dom";
-import Learn from "./Learn";
-import "../assets/style.css";
+import React from 'react';
+import {Link, Route} from 'react-router-dom';
+import Learn from './Learn';
+import '../assets/style.css';
 
 const Contact = props => (
   <div>
-    <h1 className ="header">Contact Page</h1>
-    <p className = "bodyText">
-      Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis molestie urna.
-      Aliquam semper ultrices varius. Aliquam faucibus sit amet magna a ultrices. Aenean
-      pellentesque placerat lacus imperdiet efficitur. In felis nisl, luctus non ante euismod,
-      tincidunt bibendum mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-      posuere, eget tristique dui dapibus. Maecenas fermentum elementum faucibus. Quisque nec metus
-      vestibulum, egestas massa eu, sollicitudin ipsum. Nulla facilisi. Sed ut erat ligula. Nam
-      tincidunt nunc in nibh dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-      conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at rhoncus. Etiam vel
-      condimentum magna, quis tempor nulla.
-    </p>
-    <Link to={`${props.match.url}/learn`} className="btn btn-default">
-      View My Resume
-    </Link>{" "}
-    <Link to="/contact" className="btn btn-default">
-      Learn Less
-    </Link>
-    <Route exact path={`${props.match.url}/learn`} component={Learn} />
+    <div>
+      <h1 className="header">Contact Page</h1>
+      <div className="col-md-5" />
+      <div className="col-md-2">
+        <br />
+        <img src={require('../assets/github.png')} className="logo" />
+        <br />
+        <br />
+        <img src={require('../assets/linkedin.png')} className="logo" />
+        <br />
+      </div>
+      <div className="col-md-5" />
+      <p className="bodyText">
+        If you are interested in learning more about me or have any questions,
+        please contact me at <a>danielpimen92@gmail.com</a>
+      </p>
+    </div>
+    <div className="col-md-12">
+      <Link to={`${props.match.url}/learn`} className="btn btn-default">
+        View My Resume
+      </Link>{' '}
+      <Link to="/contact" className="btn btn-default">
+        Close Resume
+      </Link>
+      <Route exact path={`${props.match.url}/learn`} component={Learn} />
+    </div>
   </div>
 );
 
